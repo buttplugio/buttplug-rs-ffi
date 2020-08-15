7d17eb99eae38b3449d3241d474cbb5c71642fd3
Kyle Machulis <kyle@nonpolynomial.com>
2020-08-15 15:42:04 -0700

    build: Have rust build enumerate flatbuf dir
    No more manual updates of the build file!

--- ffi/build.rs
+++ ffi/build.rs
@@ -1,16 +1,18 @@
-extern crate flatc_rust;  // or just `use flatc_rust;` with Rust 2018 edition.
+extern crate flatc_rust; // or just `use flatc_rust;` with Rust 2018 edition.
 
 use std::fs;
 use std::path::Path;
 
 fn main() {
-  for file in fs::read_dir("../flatbuffer_schemas/").unwrap() {
-    let file_path = file.unwrap().path();
-    println!("{}", format!("cargo:rerun-if-changed={}", file_path.display()));
-    flatc_rust::run(flatc_rust::Args {
-        inputs: &fs::read_dir("../flatbuffer_schemas/").unwrap().map(|x| x.unwrap().path().as_path()).collect(),
-        out_dir: Path::new("target/flatbuffers/"),
-        ..Default::default()
-    }).expect("flatc");
-  }
+    fs::read_dir("../flatbuffer_schemas/")
+        .unwrap()
+        .for_each(|x| {
+            let path = x.unwrap().path();
+            flatc_rust::run(flatc_rust::Args {
+                inputs: &[path.as_path()],
+                out_dir: Path::new("target/flatbuffers/"),
+                ..Default::default()
+            })
+            .expect("flatc");
+        });
 }
