d18b40be45c45784b190760fbbef419d7c2f4a33
Kyle Machulis <kyle@nonpolynomial.com>
2020-08-15 23:37:59 -0700

    chore: Build file reformat

--- ffi/build.rs
+++ ffi/build.rs
@@ -4,15 +4,15 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
-    fs::read_dir("../flatbuffer_schemas/")
-        .unwrap()
-        .for_each(|x| {
-            let path = x.unwrap().path();
-            flatc_rust::run(flatc_rust::Args {
-                inputs: &[path.as_path()],
-                out_dir: Path::new("target/flatbuffers/"),
-                ..Default::default()
-            })
-            .expect("flatc");
-        });
+  fs::read_dir("../flatbuffer_schemas/")
+    .unwrap()
+    .for_each(|x| {
+      let path = x.unwrap().path();
+      flatc_rust::run(flatc_rust::Args {
+        inputs: &[path.as_path()],
+        out_dir: Path::new("target/flatbuffers/"),
+        ..Default::default()
+      })
+      .expect("flatc");
+    });
 }
