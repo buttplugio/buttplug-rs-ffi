0c5316a347b9cc3c820115cfbcf0ad4ca12e9859
Kyle Machulis <kyle@nonpolynomial.com>
2020-09-26 22:08:28 -0700

    feat: Copy flatbuf code to protobuf, start conversion
    Start with a copy of the flatbuf code. Remove message parsing for
    create methods, meaning we can use those as raw calls.

---
+++ ffi-protobufs/src/device.rs
@@ -0,0 +1,166 @@
+use super::{
+  flatbuffer_device_generated::buttplug_ffi::{
+    get_root_as_device_message,
+    DeviceMessage,
+    DeviceMessageType,
+    LinearCmd,
+    RawReadCmd,
+    RawWriteCmd,
+    RawSubscribeCmd,
+    RawUnsubscribeCmd,
+    RotateCmd,
+    StopDeviceCmd,
+    VibrateCmd,
+  },
+  util::return_client_result,
+  FFICallback,
+};
+use buttplug::{
+  client::device::{ButtplugClientDevice, LinearCommand, RotateCommand, VibrateCommand},
+  util::async_manager,
+};
+use std::{slice, sync::Arc};
+
+pub struct ButtplugFFIDevice {
+  callback: Option<FFICallback>,
+  device: Arc<ButtplugClientDevice>,
+}
+
+impl ButtplugFFIDevice {
+  pub fn new(device: ButtplugClientDevice, callback: Option<FFICallback>) -> Self {
+    Self {
+      device: Arc::new(device),
+      callback,
+    }
+  }
+
+  pub fn parse_message(&self, buf: *const u8, buf_len: i32) {
+    let msg: &[u8];
+    unsafe {
+      msg = slice::from_raw_parts(buf, buf_len as usize);
+    }
+    let device_msg = get_root_as_device_message(msg);
+    match device_msg.message_type() {
+      DeviceMessageType::VibrateCmd => self.send_vibrate_cmd(device_msg),
+      DeviceMessageType::RotateCmd => self.send_rotate_cmd(device_msg),
+      DeviceMessageType::LinearCmd => self.send_linear_cmd(device_msg),
+      DeviceMessageType::StopDeviceCmd => self.send_stop_device_cmd(device_msg),
+      DeviceMessageType::RawReadCmd => self.send_raw_read_cmd(device_msg),
+      DeviceMessageType::RawWriteCmd => self.send_raw_write_cmd(device_msg),
+      DeviceMessageType::RawSubscribeCmd => self.send_raw_subscribe_cmd(device_msg),
+      DeviceMessageType::RawUnsubscribeCmd => self.send_raw_unsubscribe_cmd(device_msg),
+      DeviceMessageType::BatteryLevelCmd => self.send_battery_level_cmd(device_msg),
+      DeviceMessageType::RSSILevelCmd => self.send_rssi_level_cmd(device_msg),
+      DeviceMessageType::NONE => error!("Unhandled device message"),
+    };
+  }
+
+  pub fn send_vibrate_cmd(&self, msg: DeviceMessage) {
+    let vibrate_msg = msg.message_as_vibrate_cmd().unwrap();
+    let callback = self.callback.clone();
+    let params = VibrateCommand::SpeedMap(
+      vibrate_msg
+        .speeds()
+        .unwrap()
+        .into_iter()
+        .map(|x| (x.index(), x.speed()))
+        .collect(),
+    );
+    let device = self.device.clone();
+    let id = msg.id();
+    async_manager::spawn(async move {
+      return_client_result(device.vibrate(params).await, id, callback);
+    })
+    .unwrap();
+  }
+
+  pub fn send_rotate_cmd(&self, msg: DeviceMessage) {
+    let rotate_msg = msg.message_as_rotate_cmd().unwrap();
+    let callback = self.callback.clone();
+    let params = RotateCommand::RotateMap(
+      rotate_msg
+        .rotations()
+        .unwrap()
+        .into_iter()
+        .map(|x| (x.index(), (x.speed(), x.clockwise())))
+        .collect(),
+    );
+    let device = self.device.clone();
+    let id = msg.id();
+    async_manager::spawn(async move {
+      return_client_result(device.rotate(params).await, id, callback);
+    })
+    .unwrap();
+  }
+
+  pub fn send_linear_cmd(&self, msg: DeviceMessage) {
+    let linear_msg = msg.message_as_linear_cmd().unwrap();
+    let callback = self.callback.clone();
+    let params = LinearCommand::LinearMap(
+      linear_msg
+        .movements()
+        .unwrap()
+        .into_iter()
+        .map(|x| (x.index(), (x.duration(), x.position())))
+        .collect(),
+    );
+    let device = self.device.clone();
+    let id = msg.id();
+    async_manager::spawn(async move {
+      return_client_result(device.linear(params).await, id, callback);
+    })
+    .unwrap();
+  }
+
+  pub fn send_stop_device_cmd(&self, msg: DeviceMessage) {
+    let callback = self.callback.clone();
+    let device = self.device.clone();
+    let id = msg.id();
+    async_manager::spawn(async move {
+      return_client_result(device.stop().await, id, callback);
+    })
+    .unwrap();
+  }
+
+  pub fn send_raw_read_cmd(&self, msg: DeviceMessage) {
+    let raw_read_msg = msg.message_as_raw_read_cmd().unwrap();
+  }
+
+  pub fn send_raw_write_cmd(&self, msg: DeviceMessage) {
+    let raw_write_msg = msg.message_as_raw_write_cmd().unwrap();
+  }
+
+  pub fn send_raw_subscribe_cmd(&self, msg: DeviceMessage) {
+    let raw_read_msg = msg.message_as_raw_subscribe_cmd().unwrap();
+  }
+
+  pub fn send_raw_unsubscribe_cmd(&self, msg: DeviceMessage) {
+    let raw_write_msg = msg.message_as_raw_unsubscribe_cmd().unwrap();
+  }
+
+  pub fn send_battery_level_cmd(&self, msg: DeviceMessage) {
+    let callback = self.callback.clone();
+    let device = self.device.clone();
+    let id = msg.id();
+    async_manager::spawn(async move {
+      return_client_result(Ok(()), id, callback);
+    })
+    .unwrap();
+  }
+
+  pub fn send_rssi_level_cmd(&self, msg: DeviceMessage) {
+    let callback = self.callback.clone();
+    let device = self.device.clone();
+    let id = msg.id();
+    async_manager::spawn(async move {
+      return_client_result(Ok(()), id, callback);
+    })
+    .unwrap();
+  }
+}
+
+impl Drop for ButtplugFFIDevice {
+  fn drop(&mut self) {
+    info!("DROPPED RUST FFI DEVICE");
+  }
+}
---
+++ ffi-protobufs/src/export.rs
@@ -0,0 +1,84 @@
+use super::{client::ButtplugFFIClient, device::ButtplugFFIDevice, FFICallback};
+use libc::c_char;
+use std::ffi::CStr;
+
+#[no_mangle]
+pub extern "C" fn buttplug_create_client(
+  callback: Option<FFICallback>,
+  client_name_ptr: *const c_char,
+) -> *mut ButtplugFFIClient {
+  let c_str = unsafe {
+    assert!(!client_name_ptr.is_null());
+
+    CStr::from_ptr(client_name_ptr)
+  };
+
+  // If we were handed a wrong client name, just panic.
+  let client_name = c_str.to_str().unwrap();
+
+  if callback.is_none() {
+    error!("NULL CALLBACK SPECIFIED. NO MESSAGES WILL BE RETURNED, NOR WILL EVENTS BE EMITTED.");
+  }
+
+  Box::into_raw(Box::new(ButtplugFFIClient::new(&client_name, callback)))
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_free_client(ptr: *mut ButtplugFFIClient) {
+  if !ptr.is_null() {
+    unsafe {
+      Box::from_raw(ptr);
+    }
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_parse_client_message(
+  client_ptr: *mut ButtplugFFIClient,
+  buf: *const u8,
+  buf_len: i32,
+) {
+  let client = unsafe {
+    assert!(!client_ptr.is_null());
+    &mut *client_ptr
+  };
+  client.parse_message(buf, buf_len);
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_create_device(
+  client_ptr: *mut ButtplugFFIClient,
+  device_index: u32,
+) -> *mut ButtplugFFIDevice {
+  let client = unsafe {
+    assert!(!client_ptr.is_null());
+    &mut *client_ptr
+  };
+  if let Some(device) = client.get_device(device_index) {
+    Box::into_raw(Box::new(device))
+  } else {
+    std::ptr::null::<*mut ButtplugFFIDevice>() as *mut ButtplugFFIDevice
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_parse_device_message(
+  device_ptr: *mut ButtplugFFIDevice,
+  buf: *const u8,
+  buf_len: i32,
+) {
+  let device = unsafe {
+    assert!(!device_ptr.is_null());
+    &mut *device_ptr
+  };
+  device.parse_message(buf, buf_len);
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_free_device(ptr: *mut ButtplugFFIDevice) {
+  if !ptr.is_null() {
+    unsafe {
+      Box::from_raw(ptr);
+    }
+  }
+}
