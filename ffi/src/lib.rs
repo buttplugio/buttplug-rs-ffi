6c27bc36d32ac49b0cc011e67eee4a5f34fb08e2
Kyle Machulis <kyle@nonpolynomial.com>
2020-10-26 14:38:49 -0700

    feat (Core): Implement log emitters in core FFI
    Add ability to set up and emit message from tracing via FFI callbacks.

--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -1,12 +1,20 @@
 #[macro_use]
 extern crate tracing;
 
+// Uncomment this once single type filter lands in tracing
+/*
+#[macro_use]
+extern crate lazy_static;
+*/
+
 mod client;
 mod device;
 mod export;
 mod util;
 mod pbufs;
+mod logging;
 
 pub use export::*;
+pub use logging::*;
 
 type FFICallback = extern "C" fn(*const u8, u32);
---
+++ ffi/src/logging.rs
@@ -0,0 +1,55 @@
+use buttplug::util::logging::ChannelWriter;
+use std::{
+  ffi::{CString, CStr},
+  str::FromStr
+};
+use libc::c_char;
+use async_std::task;
+use async_channel::bounded;
+use tracing::{Level};
+use futures::StreamExt;
+
+// Uncomment this once single type filter lands in tracing.
+/*
+use tracing_subscriber::{
+  EnvFilter,
+  reload::Handle,
+};
+
+lazy_static! {
+  static ref LOG_FILTER_HANDLE: Option<Handle<EnvFilter>> = None;
+}
+*/
+
+type LogFFICallback = extern "C" fn(*const c_char);
+
+#[no_mangle]
+pub extern "C" fn buttplug_set_log_callback(callback: LogFFICallback, max_level_cchar: *const c_char) {
+  let (sender, mut receiver) = bounded(256);
+  task::spawn(async move {
+    while let Some(msg) = receiver.next().await {
+      let str = CString::new(msg).expect("Issue with conversion of log message.");
+      callback(str.as_ptr());
+    }
+  });
+  let max_level_cstr: &CStr = unsafe { CStr::from_ptr(max_level_cchar) };
+  let subscriber = tracing_subscriber::fmt()
+    .json()
+    .with_ansi(false)
+    .with_writer(ChannelWriter::new(sender))
+    .with_env_filter(max_level_cstr.to_str().unwrap())
+    .with_filter_reloading();
+
+  // Uncomment this once single type filter lands in tracing.
+  /*
+    let handle = subscriber.reload_handle();
+    let wrapper = FilterWrapper { handle };
+  */
+  subscriber.init();
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_set_log_level(max_level_cchar: *const c_char) {
+  let max_level_cstr: &CStr = unsafe { CStr::from_ptr(max_level_cchar) };
+  let max_level: Level = Level::from_str(max_level_cstr.to_str().unwrap()).unwrap();
+}
