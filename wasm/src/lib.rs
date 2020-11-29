6a0143495f738aff4eeb005f23e6f0936780f420
Kyle Machulis <kyle@nonpolynomial.com>
2020-11-29 15:15:53 -0800

    build: Add more descriptions and features for builds
    Add more project identifying info for npm.

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -27,7 +27,7 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 pub fn start() -> Result<(), JsValue> {
   // print pretty errors in wasm https://github.com/rustwasm/console_error_panic_hook
   // This is not needed for tracing_wasm to work, but it is a common tool for getting proper error line numbers for panics.
-  console_error_panic_hook::set_once();
+  utils::set_panic_hook();
 
   // Add this line:
   tracing_wasm::set_as_global_default();
