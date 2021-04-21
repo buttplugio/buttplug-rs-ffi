7093f64b9d7dd7b09a52545fe9c32077fa442429
Kitlith <kitlith@kitl.pw>
2021-04-20 17:09:33 -0700

    Use dev branch, return futures, kill sorter.

--- java/lib/src/main/java/io/buttplug/ffi/ButtplugFFIClient.java
+++ java/lib/src/main/java/io/buttplug/ffi/ButtplugFFIClient.java
@@ -2,22 +2,59 @@ package io.buttplug.ffi;
 
 import jnr.ffi.Pointer;
 
+import java.nio.ByteBuffer;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class ButtplugFFIClient implements AutoCloseable {
     private final ButtplugFFI.LibButtplug buttplug;
-    private final Pointer pointer;
+    private Pointer pointer;
+    // reference kept to prevent garbage collection.
+    private ButtplugFFI.FFICallback system_callback;
+    // callback references used to preserve callbacks until they have been called.
+    private final Set<ButtplugFFI.FFICallback> pending_callbacks = ConcurrentHashMap.newKeySet();
 
-    ButtplugFFIClient(ButtplugFFI.LibButtplug buttplug, Pointer pointer) {
+    ButtplugFFIClient(ButtplugFFI.LibButtplug buttplug, Pointer pointer, ButtplugFFI.FFICallback system_callback) {
         this.buttplug = buttplug;
         this.pointer = pointer;
+        this.system_callback = system_callback;
     }
 
+    // TODO: fail-safe on garbage collection before client is freed?
     @Override
-    public void close() throws Exception {
+    public void close() {
         buttplug.buttplug_free_client(pointer);
+        this.system_callback = null;
+        this.pointer = null;
     }
 
-    public void parse_client_message(byte[] buf) {
-        buttplug.buttplug_parse_client_message(pointer, buf, buf.length);
+    // todo: future vs callback?
+    CompletableFuture<byte[]> send_protobuf_message(byte[] buf) {
+        CompletableFuture<byte[]> future = new CompletableFuture<>();
+
+        // TODO: hold weak instead of strong reference to future in callback?
+        ButtplugFFI.FFICallback cb = new ButtplugFFI.FFICallback() {
+            @Override
+            public void callback(Pointer ctx, ByteBuffer ptr, int len) {
+                try {
+                    byte[] buf = new byte[len];
+                    ptr.get(buf);
+                    future.complete(buf);
+                } catch (Throwable ex) {
+                    future.completeExceptionally(ex);
+                } finally {
+                    // we're done, don't need to hold onto a reference anymore.
+                    pending_callbacks.remove(this);
+                }
+            }
+        };
+
+        // hold onto a reference to the callback until it's been called
+        pending_callbacks.add(cb);
+        buttplug.buttplug_client_protobuf_message(pointer, buf, buf.length, cb, null);
+
+        return future;
     }
 
     public ButtplugFFIDevice create_device(int index) {
