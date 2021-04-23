7413df26a91bdf51014114f6b444a9fec0d17f02
Kitlith <kitlith@kitl.pw>
2021-04-22 18:32:54 -0700

    write the skeleton of what a non-protobuf api would look like
    everything is unimplemented, and i'm not actually sure about my usage of
    generics with the callback.
    
    we should probably introduce cbindgen as well.

--- ffi/src/export.rs
+++ ffi/src/export.rs
@@ -4,6 +4,9 @@ use super::{
   client::ButtplugFFIClient,
   device::ButtplugFFIDevice, 
   FFICallback,
+  FFIResultCallback,
+  FFIResult,
+  FFISystemCallback,
   logging::{LogFFICallback, ButtplugFFILogHandle}
 };
 use libc::c_char;
@@ -14,6 +17,7 @@ use std::{
 };
 use tracing_subscriber;
 use tokio::runtime::Runtime;
+use buttplug::device::Endpoint;
 
 lazy_static! {
   static ref RUNTIME: Mutex<Weak<tokio::runtime::Runtime>> = Mutex::new(Weak::new());
@@ -35,6 +39,22 @@ fn get_or_create_runtime() -> Arc<Runtime> {
     }
   }
 }
+
+#[no_mangle]
+pub extern "C" fn buttplug_create_client(
+  client_name_ptr: *const c_char,
+  callback: FFISystemCallback,
+  callback_context: FFICallbackContext
+) -> *mut ButtplugFFIClient {
+  let c_str = unsafe {
+    assert!(!client_name_ptr.is_null());
+
+    CStr::from_ptr(client_name_ptr)
+  };
+
+  unimplemented!();
+}
+
 
 #[no_mangle]
 pub extern "C" fn buttplug_create_protobuf_client(
@@ -62,6 +82,109 @@ pub extern "C" fn buttplug_free_client(ptr: *mut ButtplugFFIClient) {
     }
   }
 }
+
+// TODO: is there a pre-existing struct for this?
+#[repr(C)]
+pub struct LocalConnectConfig {
+  server_name: *const c_char,
+  pub max_ping_time: u32,
+  pub allow_raw_messages: bool,
+  device_configuration_json: *const c_char,
+  user_device_configuration_json: *const c_char,
+  // TODO: use bitflags
+  pub comm_manager_types: u32
+}
+
+impl LocalConnectConfig {
+  fn server_name(&self) -> &CStr {
+    unsafe {
+      CStr::from_ptr(self.server_name)
+    }
+  }
+
+  fn device_configuration_json(&self) -> &CStr {
+    unsafe {
+      CStr::from_ptr(self.device_configuration_json)
+    }
+  }
+
+  fn user_device_configuration_json(&self) -> &CStr {
+    unsafe {
+      CStr::from_ptr(self.user_device_configuration_json)
+    }
+  }
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_client_connect_local(
+  client_ptr: &mut ButtplugFFIClient,
+  callback: FFIResultCallback<()>,
+  callback_context: FFICallbackContext,
+
+  config: &LocalConnectConfig
+) {
+  unimplemented!();
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_client_connect_websocket(
+  client_ptr: &mut ButtplugFFIClient,
+  callback: FFIResultCallback<()>,
+  callback_context: FFICallbackContext,
+
+  address: *const c_char,
+  bypass_cert_verification: bool
+)  {
+  let address = unsafe {
+    CStr::from_ptr(address)
+  };
+  unimplemented!();
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_client_start_scanning(
+  client_ptr: &mut ButtplugFFIClient,
+  callback: FFIResultCallback<()>,
+  callback_context: FFICallbackContext,
+) {
+  unimplemented!();
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_client_stop_scanning(
+  client_ptr: &mut ButtplugFFIClient,
+  callback: FFIResultCallback<()>,
+  callback_context: FFICallbackContext,
+) {
+  unimplemented!();
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_client_stop_all_devices(
+  client_ptr: &mut ButtplugFFIClient,
+  callback: FFIResultCallback<()>,
+  callback_context: FFICallbackContext,
+) {
+  unimplemented!();
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_client_disconnect(
+  client_ptr: &mut ButtplugFFIClient,
+  callback: FFIResultCallback<()>,
+  callback_context: FFICallbackContext,
+) {
+  unimplemented!();
+}
+#[no_mangle]
+pub extern "C" fn buttplug_client_ping(
+  client_ptr: &mut ButtplugFFIClient,
+  callback: FFIResultCallback<()>,
+  callback_context: FFICallbackContext,
+) {
+  unimplemented!();
+}
+
 
 #[no_mangle]
 pub extern "C" fn buttplug_client_protobuf_message(
@@ -96,6 +219,184 @@ pub extern "C" fn buttplug_create_device(
   } else {
     std::ptr::null::<*mut ButtplugFFIDevice>() as *mut ButtplugFFIDevice
   }
+}
+
+#[repr(C)]
+pub struct VibrateComponent {
+  index: u32,
+  speed: f64
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_device_vibrate_cmd(
+  device_ptr: &mut ButtplugFFIClient,
+  callback: FFIResultCallback<()>,
+  callback_context: FFICallbackContext,
+
+  components: *const VibrateComponent,
+  components_len: usize
+) {
+  let components = unsafe {
+    slice::from_raw_parts(components, components_len)
+  };
+  unimplemented!();
+}
+
+#[repr(C)]
+pub struct RotateComponent {
+  index: u32,
+  speed: f64,
+  clockwise: bool
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_device_rotate_cmd(
+  device_ptr: &mut ButtplugFFIClient,
+  callback: FFIResultCallback<()>,
+  callback_context: FFICallbackContext,
+
+  components: *const RotateComponent,
+  components_len: usize
+) {
+  let components = unsafe {
+    slice::from_raw_parts(components, components_len)
+  };
+  unimplemented!();
+}
+
+#[repr(C)]
+pub struct LinearComponent {
+  index: u32,
+  duration: u32,
+  position: f64
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_device_linear_cmd(
+  device_ptr: &mut ButtplugFFIClient,
+  callback: FFIResultCallback<()>,
+  callback_context: FFICallbackContext,
+
+  components: *const LinearComponent,
+  components_len: usize
+) {
+  let components = unsafe {
+    slice::from_raw_parts(components, components_len)
+  };
+  unimplemented!();
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_device_stop_cmd(
+  device_ptr: &mut ButtplugFFIClient,
+  callback: FFIResultCallback<()>,
+  callback_context: FFICallbackContext,
+) {
+  unimplemented!();
+}
+
+#[repr(C)]
+pub struct RawReadResponse {
+  // TODO: do we actually need to send the index or endpoint as part of the response?
+  index: u32,
+  // TODO: make endpoint enum repr(u8)
+  endpoint: Endpoint,
+  data: *const u8,
+  data_len: usize
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_device_raw_read_cmd(
+  device_ptr: &mut ButtplugFFIClient,
+  callback: FFIResultCallback<RawReadResponse>,
+  callback_context: FFICallbackContext,
+
+  // TODO: make endpoint enum repr(u8)
+  endpoint: Endpoint,
+  data: *const u8,
+  data_len: usize,
+  expected_len: usize,
+  timeout: u32
+) {
+  let data = unsafe {
+    slice::from_raw_parts(data, data_len)
+  };
+  unimplemented!();
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_device_raw_write_cmd(
+  device_ptr: &mut ButtplugFFIClient,
+  callback: FFIResultCallback<()>,
+  callback_context: FFICallbackContext,
+
+  // TODO: make actual endpoint struct repr(C, u8)?
+  endpoint: u8,
+  data: *const u8,
+  data_len: usize,
+  write_with_response: bool
+) {
+  let data = unsafe {
+    slice::from_raw_parts(data, data_len)
+  };
+  unimplemented!();
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_device_raw_subscribe_cmd(
+  device_ptr: &mut ButtplugFFIClient,
+  callback: FFIResultCallback<()>,
+  callback_context: FFICallbackContext,
+
+  // TODO: make actual endpoint struct repr(C, u8)?
+  endpoint: u8
+) {
+  unimplemented!();
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_device_raw_unsubscribe_cmd(
+  device_ptr: &mut ButtplugFFIClient,
+  callback: FFIResultCallback<()>,
+  callback_context: FFICallbackContext,
+
+  // TODO: make actual endpoint struct repr(C, u8)?
+  endpoint: u8
+) {
+  unimplemented!();
+}
+
+#[repr(C)]
+pub struct BatteryLevelResponse {
+  // TODO: is index necessary? could just replace whole struct with f64 if not.
+  index: u32,
+  reading: f64
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_device_battery_level_cmd(
+  device_ptr: &mut ButtplugFFIClient,
+  callback: FFIResultCallback<BatteryLevelResponse>,
+  callback_context: FFICallbackContext,
+) {
+  unimplemented!();
+}
+
+#[repr(C)]
+pub struct RSSILevelResponse {
+  // TODO: is index necessary? could just replace whole struct with i32 if not.
+  index: u32,
+  reading: i32
+}
+
+#[no_mangle]
+pub extern "C" fn buttplug_device_rssi_level_cmd(
+  device_ptr: &mut ButtplugFFIClient,
+  // TODO: non-protobuf callback type
+  callback: FFIResultCallback<RSSILevelResponse>,
+  callback_context: FFICallbackContext,
+) {
+  unimplemented!();
 }
 
 #[no_mangle]
