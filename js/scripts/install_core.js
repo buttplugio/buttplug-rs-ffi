232a961e5da7f9a9ec28636d84de6035beeda062
itsame-luigi <84438007+itsame-luigi@users.noreply.github.com>
2021-08-09 16:21:05 -0700

    Update to package and install scripts

--- js/scripts/install_core.js
+++ js/scripts/install_core.js
@@ -4,15 +4,17 @@ import https from "https";
 import os from "os";
 import path from "path";
 import { copyFile, mkdir, mkdtemp, readdir, readFile, rm, writeFile } from "fs/promises";
-import { exec } from "child_process";
+import { exec, spawn } from "child_process";
 import { pipeline } from "stream/promises";
 import { fileURLToPath, URL } from "url";
-import { createWriteStream } from "fs";
+import { createWriteStream, existsSync } from "fs";
 import util from "util";
 
 const MAX_REDIRECTS = 10;
 const execAsync = util.promisify(exec);
 const packageDir = fileURLToPath(new URL("..", import.meta.url));
+const repoDir = fileURLToPath(new URL("../..", import.meta.url));
+const ffiDir = fileURLToPath(new URL("../../ffi", import.meta.url));
 
 /**
  * @typedef ParsedArgs
@@ -20,6 +22,8 @@ const packageDir = fileURLToPath(new URL("..", import.meta.url));
  * @property {string} [platform]
  * @property {string} [arch]
  * @property {boolean} [force]
+ * @property {boolean} [build]
+ * @property {boolean} [copy]
  * @property {string} [from]
  */
 
@@ -40,19 +44,42 @@ const packageDir = fileURLToPath(new URL("..", import.meta.url));
 function parseArgs() {
     /** @type {ParsedArgs} */
     const parsedArgs = {
-        force: false,
-        version: undefined,
-        platform: undefined,
-        arch: undefined,
-        from: undefined,
+        force: !!process.env.BUTTPLUG_FFI_INSTALL_CORE_FORCE,
+        version: process.env.BUTTPLUG_FFI_INSTALL_CORE_VERSION,
+        platform: process.env.BUTTPLUG_FFI_INSTALL_CORE_PLATFORM,
+        arch: process.env.BUTTPLUG_FFI_INSTALL_CORE_ARCH,
+        from: process.env.BUTTPLUG_FFI_INSTALL_CORE_FROM,
+        copy: !!process.env.BUTTPLUG_FFI_INSTALL_CORE_COPY,
+        build: !!process.env.BUTTPLUG_FFI_INSTALL_CORE_BUILD,
     };
     const args = process.argv.slice(2);
     let arg;
     while ((arg = args.shift()) !== undefined) {
         const argLower = arg.toLowerCase();
         switch (argLower) {
+            case "--version":
+                if (parsedArgs.build) throw new Error(`Argument '--version' cannot be specified with '--build'.`);
+                break;
+            case "--from":
+                if (parsedArgs.build) throw new Error(`Argument '--from' cannot be specified with '--build'.`);
+                break;
+            case "--build":
+                if (parsedArgs.version) throw new Error(`Argument '--build' cannot be specified with '--version'.`);
+                if (parsedArgs.from) throw new Error(`Argument '--build' cannot be specified with '--from'.`);
+                break;
+        }
+        switch (argLower) {
+            case "--help":
+            case "-h":
+            case "-?":
+                printHelp();
+                process.exit(0);
             case "--force":
-            case "--no-force": {
+            case "--no-force":
+            case "--copy":
+            case "--no-copy":
+            case "--build":
+            case "--no-build": {
                 const value = !argLower.startsWith("--no-");
                 parsedArgs[argLower.slice(value ? 2 : 5)] = value;
                 break;
@@ -71,6 +98,41 @@ function parseArgs() {
         }
     }
     return parsedArgs;
+}
+
+function printHelp() {
+    console.log([
+        `Downloads the FFI core library from the web.`,
+        ``,
+        `usage: node scripts/install_core.js [options]`,
+        ``,
+        `options:`,
+        `    --version <version>    Specifies the ffi core library version.`,
+        `                           Defaults to the value of 'ffi-version' in package.json.`,
+        `                           Cannot be used with '--build'.`,
+        `    --platform <platform>  Specifies the ffi core library platform.`,
+        `                           One of: 'darwin', 'linux', or 'win32'.`,
+        `                           Defaults to the host platform (if supported).`,
+        `    --arch <arch>          Specifies the ffi core library processor architecture.`,
+        `                           One of: 'x64'.`,
+        `                           Defaults to the host's architecture (if supported).`,
+        `    --copy                 Copies files from a local directory rather than from the web.`,
+        `                           Use '--from' to override the directory.`,
+        `                           Defaults to the rust ffi build output directory when run within the`,
+        `                           GIT repository.`,
+        `    --from <dir>           Copies ffi core library files from <dir> rather than downloading from the web.`,
+        `                           Implies '--copy'.`,
+        `                           Cannot be used with '--build'.`,
+        `    --build                Builds the ffi core library when run within the GIT repo.`,
+        `                           Implies '--copy'.`,
+        `                           Cannot be used with '--from' or '--version'.`,
+        `                           NOTE: If you are building a <platform> other than your host platform, you must`,
+        `                           already have the platform-specific rust targets installed as well as any necessary`,
+        `                           cross-platform build tools`,
+        `    --force                Force overwrite of ffi core library files.`,
+        ` -h --help                 Prints this message.`,
+        ``,
+    ].join("\n"));
 }
 
 /**
@@ -263,6 +325,60 @@ async function copyPackage(sourceDir, release, cacheInfo) {
     }
 }
 
+/**
+ * @param {Release} release
+ */
+function getRustTarget(release) {
+    if (release.platform !== process.platform ||
+        release.arch !== process.arch) {
+        switch (release.platform) {
+            case "darwin":
+                return "x86_64-apple-darwin";
+            case "linux":
+                return "x86_64-unknown-linux-gnu";
+            case "win32":
+                return "x86_64-pc-windows-msvc";
+        }
+    }
+}
+
+/**
+ * @param {Release} release
+ * @param {"x86_64-apple-darwin" | "x86_64-unknown-linux-gnu" | "x86_64-pc-windows-msvc" | undefined} target
+ */
+function getOutputDir(release, target = getRustTarget(release)) {
+    if (path.basename(repoDir) === "node_modules" || !existsSync(path.join(ffiDir, "Cargo.toml"))) {
+        throw new Error(`Cannot find rust components.`);
+    }
+    return path.join(ffiDir, target ? `target/${target}/release` : "target/release");
+}
+
+/**
+ * @param {Release} release
+ */
+async function buildRust(release) {
+    const target = getRustTarget(release);
+    const outDir = getOutputDir(release, target);
+
+    const args = target ?
+        ["build", "--release", "--target", target] :
+        ["build", "--release"];
+
+    console.log("building ffi...");
+
+    /** @type {number | null} */
+    const exitCode = await new Promise((resolve, reject) => {
+        const proc = spawn("cargo", args, { cwd: ffiDir, stdio: "inherit" });
+        proc.on("error", reject);
+        proc.on("exit", resolve);
+    });
+
+    if (exitCode) throw new Error(`Process exited with code: ${exitCode}`);
+
+    await copyPackage(outDir, release);
+    console.log("ffi library updated");
+}
+
 async function main() {
     const args = parseArgs();
     const version = args.version || await readFfiVersionFromPackageJson();
@@ -274,7 +390,11 @@ async function main() {
 
     /** @type {Release} */
     const release = { version, platform, arch };
-    if (args.from) {
+    if (args.build) {
+        await buildRust(release);
+    }
+    else if (args.copy || args.from) {
+        args.from ??= getOutputDir(release);
         await copyPackage(args.from, release);
         console.log("ffi library updated");
     }
